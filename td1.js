5d77811f5f1b8c1fd964d622a3a1984128c62fe5
Alex M <45000671+Alexti2d@users.noreply.github.com>
2022-01-07 10:48:06 +0100

    Vitesse

--- td1.js
+++ td1.js
@@ -1,18 +1,21 @@
 navigator.geolocation.getCurrentPosition(position => {
 
     var { latitude, longitude } = position.coords;
-    var altitude = position.altitude;
     console.log(latitude, longitude)
 
     $("#Latitude").text("Latitude : " + latitude);
     $("#Longitude").text("Longitude : " + longitude);
-    $("#Altitude").text("Altitude : " + altitude);
 
   });
 
   const watchId = navigator.geolocation.watchPosition(position => {
     var { latitude, longitude } = position.coords;
-    
+    var time = Date.now();
+    var date = new Date(time)
+    var vitesse = position.vitesse;
+
+    $("#Timestamp").text("Timestamp : " + date);
+    $("#Vitesse").text("Vitesse : " + vitesse);
     $("#Latitude2").text("Latitude : " + latitude);
     $("#Longitude2").text("Longitude : " + longitude);
 
