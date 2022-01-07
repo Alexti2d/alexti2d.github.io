7a550f7be41857b083471fc2277b6a75203c648c
Alex M <45000671+Alexti2d@users.noreply.github.com>
2022-01-07 11:41:26 +0100

    architecture

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ $("#Nav").append(`
     <h3 class="float-md-start mb-0">Alex M</h3>
     <nav class="nav nav-masthead justify-content-center float-md-end">
       <a class="nav-link active" aria-current="page" href="/">Home</a>
-      <a class="nav-link" href="td1.html">TD1</a>
-      <a class="nav-link" href="td2.html">TD2</a>
-      <a class="nav-link" href="td3.html">TD3</a>
-      <a class="nav-link" href="td4.html">TD4</a>
+      <a class="nav-link" href="../td/td1.html">TD1</a>
+      <a class="nav-link" href="../td/td2.html">TD2</a>
+      <a class="nav-link" href="../td/td3.html">TD3</a>
+      <a class="nav-link" href="../td/td4.html">TD4</a>
       <a class="nav-link" href="boussole.html">Boussole</a>
     </nav>
   </div>
