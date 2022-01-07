e166e5ae9c24ce7b33af99bae9bfc4d87f9b5522
Alex M <45000671+Alexti2d@users.noreply.github.com>
2022-01-07 09:30:15 +0100

    Menu

---
+++ main.js
@@ -0,0 +1,16 @@
+$("#Nav").append(`
+
+<header class="mb-auto">
+  <div>
+    <h3 class="float-md-start mb-0">Alex M</h3>
+    <nav class="nav nav-masthead justify-content-center float-md-end">
+      <a class="nav-link active" aria-current="page" href="/">Home</a>
+      <a class="nav-link" href="td1.html">TD1</a>
+      <a class="nav-link" href="td2.html">TD2</a>
+      <a class="nav-link" href="td3.html">TD3</a>
+      <a class="nav-link" href="td4.html">TD4</a>
+      <a class="nav-link" href="boussole.html">Boussole</a>
+    </nav>
+  </div>
+</header>
+`)
