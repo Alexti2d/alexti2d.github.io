8010750ab0bc58829e67cda80f03d831d6dfdb74
Alex M <45000671+Alexti2d@users.noreply.github.com>
2022-02-16 09:28:51 +0100

    Update projet.js

--- td/projet.js
+++ td/projet.js
@@ -46,9 +46,9 @@ function deviceOrientationListener(event) {
   $("#CameraY").text("Orientation beta : " + Math.round(event.beta));
   $("#CameraZ").text("Orientation gamma : " + Math.round(event.gamma));
 
-    mesh.rotation.y = event.alpha * Math.PI/180;
+    mesh.rotation.z = event.alpha * Math.PI/180;
     mesh2.rotation.x = event.beta * Math.PI/180;
-    mesh3.rotation.z = event.gamma * Math.PI/180;
+    mesh3.rotation.y = event.gamma * Math.PI/180;
   }
 
   
