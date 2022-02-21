ae05df0fe0990e1715604cc3a770a28a673b7107
Alex M <45000671+Alexti2d@users.noreply.github.com>
2022-02-21 15:20:08 +0100

    pwa

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ $("#Nav").append(`
     <h3 class="float-md-start mb-0">Alex M</h3>
     <nav class="nav nav-masthead justify-content-center float-md-end">
       <a class="nav-link active" aria-current="page" href="/">Home</a>
-      <a class="nav-link" href="../td/td1.html">TD1</a>
-      <a class="nav-link" href="../td/td2.html">TD2</a>
-      <a class="nav-link" href="../td/td3.html">TD3</a>
-      <a class="nav-link" href="../td/td4.html">TD4</a>
-      <a class="nav-link" href="../td/projet.html">Projet</a>
+      <a class="nav-link" href="../../td/td1.html">TD1</a>
+      <a class="nav-link" href="../../td/td2.html">TD2</a>
+      <a class="nav-link" href="../../td/td3.html">TD3</a>
+      <a class="nav-link" href="../../td/td4.html">TD4</a>
+      <a class="nav-link" href="../../td/projet.html">Projet</a>
+      <a class="nav-link" href="../../td/pwa/pwa.html">PWA</a>
     </nav>
   </div>
 </header>
