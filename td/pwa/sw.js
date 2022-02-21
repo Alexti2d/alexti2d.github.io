2edd7d9e5f5e72d449dff4adb1ab42c1bcac2d37
Alex M <45000671+Alexti2d@users.noreply.github.com>
2022-02-21 16:47:59 +0100

    Update sw.js

--- td/pwa/sw.js
+++ td/pwa/sw.js
@@ -2,7 +2,16 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('v1').then((cache) => {
         return cache.addAll([
-          '../pwa/'
+            './pwa.html',
+            './pwa.css',
+            './pwa.js',
+            './sw.js',
+            './icon-192x192.png',
+            './icon-256x256.png',
+            './icon-384x384.png',
+            './icon-512x512.png',
+            './manifest.webmanifest',
+            '../pwa/'
         ]);
       })
     );
