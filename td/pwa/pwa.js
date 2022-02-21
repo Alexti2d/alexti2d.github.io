0d4fb7ab068bbee5efb9f5ef543af0761a7b0359
Alex M <45000671+Alexti2d@users.noreply.github.com>
2022-02-21 17:03:11 +0100

    test

--- td/pwa/pwa.js
+++ td/pwa/pwa.js
@@ -3,24 +3,6 @@ if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('./sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        self.addEventListener('install', (event) => {
-            event.waitUntil(
-              caches.open('v2').then((cache) => {
-                return cache.addAll([
-                    './pwa.html',
-                    './pwa.css',
-                    './pwa.js',
-                    './sw.js',
-                    './icon-192x192.png',
-                    './icon-256x256.png',
-                    './icon-384x384.png',
-                    './icon-512x512.png',
-                    './manifest.webmanifest',
-                    '../pwa/'
-                ]);
-              })
-            );
-          });
       }, function(err) {
         // registration failed :
         console.log('ServiceWorker registration failed: ', err);
