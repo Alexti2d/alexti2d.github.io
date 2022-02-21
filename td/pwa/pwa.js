bb7f585c4c6f6c79383067dc161a51514353d33f
Alex M <45000671+Alexti2d@users.noreply.github.com>
2022-02-21 17:06:56 +0100

    test2

--- td/pwa/pwa.js
+++ td/pwa/pwa.js
@@ -1,13 +1,5 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('./sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
-    
-  }
-  
+window.addEventListener("load", () => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("./sw.js");
+    }
+  });
